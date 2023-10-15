485aa98fdb0ca085cf544d1d02a9f813a249ac48
PC-AGR-ROBERT\robertdccaetano <robert@agenciarubidea.com.br>
2023-10-15 17:12:54 -0300

    Criando _controller que o MultiStream do usuário logado
    Criando método _updateUser que recebe um User do Chat para poder settalo como _currentUser e posteriormente adicionar na Stream a etapa em que ele está passando pelo método UpdateUser
    
    Implementando o método signUp criando um usuario novo com ID randomico de double e inserindo ele no Map (pseudo DB) da aplicação em memória para salvar como novo usuário cadastrado

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:app_chat_fire_base/core/models/chat_user.dart';
 import 'dart:io';
 
@@ -6,6 +9,12 @@ import 'package:app_chat_fire_base/core/services/auth/auth_service.dart';
 class AuthMockService implements AuthService {
   static Map<String, ChatUser> _users = {};
   static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
 
   @override
   ChatUser? get currentUser {
@@ -14,24 +23,36 @@ class AuthMockService implements AuthService {
 
   @override
   Future<void> login(String email, String password) async {
-    // TODO: implement login
-    throw UnimplementedError();
+    _updateUser(_users[email]);
   }
 
   @override
   Future<void> logout() async {
-    // TODO: implement logout
-    throw UnimplementedError();
+    _updateUser(null);
   }
 
   @override
   Future<void> signUp(
       String name, String email, String password, File image) async {
-    // TODO: implement signUp
-    throw UnimplementedError();
+    final newUser = ChatUser(
+        id: Random().nextDouble().toString(),
+        name: name,
+        email: email,
+        imageUrl: image.path);
+
+    // Insere na lista/Map se ausente
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
   }
 
   @override
   // TODO: implement userChanges
-  Stream<ChatUser?> get userChanges {}
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
