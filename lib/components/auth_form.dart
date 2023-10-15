33f6c71546a7d82c85e283b8e5c05ec91f23dc8c
PC-AGR-ROBERT\robertdccaetano <robert@agenciarubidea.com.br>
2023-10-14 22:30:29 -0300

    Personalizando componente de form da tela de autenticação
    Configurando para manter os dados com initialValue  e onChanged no TextFormField.
    
    Usando estado para alternar entre Modo.Login ou Modo.SignUp

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,6 +1,19 @@
+import 'package:app_chat_fire_base/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _authFormData = AuthFormData();
+
+  void _submit() {
+    _formKey.currentState?.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -8,31 +21,48 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+            key: _formKey,
             child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Nome'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'E-mail'),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Senha'),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Entrar'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Criar uma nova conta?'),
-            ),
-          ],
-        )),
+              children: [
+                if (_authFormData.isSignup)
+                  TextFormField(
+                    key: ValueKey('name'),
+                    initialValue: _authFormData.name,
+                    onChanged: (value) => _authFormData.name = value,
+                    decoration: InputDecoration(labelText: 'Nome'),
+                  ),
+                TextFormField(
+                  key: ValueKey('email'),
+                  initialValue: _authFormData.email,
+                  onChanged: (value) => _authFormData.email = value,
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                ),
+                TextFormField(
+                  key: ValueKey('password'),
+                  initialValue: _authFormData.password,
+                  onChanged: (value) => _authFormData.password = value,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Senha'),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                ElevatedButton(
+                  onPressed: _submit,
+                  child: Text(_authFormData.isLogin ? 'Entrar' : 'Cadastrar'),
+                ),
+                TextButton(
+                  child: Text(_authFormData.isLogin
+                      ? 'Criar uma nova conta?'
+                      : 'Já possui conta?'),
+                  onPressed: () {
+                    setState(() {
+                      _authFormData.toogleAuthMode();
+                    });
+                  },
+                ),
+              ],
+            )),
       ),
     );
   }
